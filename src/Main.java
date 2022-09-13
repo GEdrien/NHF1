35048656ea27733082cb54398d782521bebc78b4
Adrian Gilanyi <60935462+GEdrien@users.noreply.github.com>
2022-09-13 13:57:43 +0200

    Input added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("For the first commit");
+        Scanner my_scanner = new Scanner(System.in);
 
-        my_method();
+        char my_char = 'O';
+        while(my_char != 'X'){
+            my_method();
+            System.out.println("Are you wants to quit? (X)");
+            my_char = my_scanner.next().charAt(0);
+        }
+        System.out.println("Exit...");
     }
 
     static void my_method(){
