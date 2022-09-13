fed7ce807111c202ac6f0b4c78af7e3bf87b8184
Adrian Gilanyi <60935462+GEdrien@users.noreply.github.com>
2022-09-13 13:47:59 +0200

    Dice roll mechanic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("For the first commit");
+
+        my_method();
+    }
+
+    static void my_method(){
+        Random my_random = new Random();
+        int x = my_random.nextInt(6)+1;
+        System.out.println("Dice roll: " +  x);
     }
 }
